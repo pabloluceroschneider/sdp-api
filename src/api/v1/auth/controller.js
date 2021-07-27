533bc94c7fc76a4a937ec234655c0df2e7616685
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2021-07-26 23:33:47 -0300

    init configs

--- src/api/v1/auth/controller.js
+++ src/api/v1/auth/controller.js
@@ -1,20 +1,43 @@
 const userService = require('../users/services');
+const configurationsService = require('../app_configurations/services');
+const companiesService = require('../companies/services');
+const baseplansService = require('../baseplans/services');
+const productsService = require('../products/services');
 
 class Auth {
 	login = async (req, res, next) => {
 		try {
-			const users = await userService.findByAttribute({
+			const logged_users = await userService.findByAttribute({
 				attribute: 'username',
 				value: req.body.username
 			});
 
-			if (!users) {
+			if (!logged_users) {
 				res.status(400).send('User not found.');
 			}
-			const [ user ] = users;
+			const [ user ] = logged_users;
 			const auth = user.password === req.body.password;
+			
+			const user_permissions = user.permissions;
 
-			auth ? res.json([ ...user.permissions ]) : res.status(400).send('Password incorrect.');
+			// configurations
+			const { permissions, status } = await configurationsService.findOne();
+			const users = await userService.find();
+			const companies = await companiesService.find();
+			const baseplans = await baseplansService.find();
+			const products = await productsService.find();
+
+			const response = {
+				user_permissions,
+				status,
+				permissions,
+				users,
+				companies,
+				baseplans,
+				products,
+			};
+
+			auth ? res.json(response) : res.status(400).send('Password incorrect.');
 		} catch (error) {
 			res.send(error);
 		}
