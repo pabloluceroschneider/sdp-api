873b482cf228099cd868bea464225c69831bbb7f
pabloluceroschneider <pabloluceroschneider@gmail.com>
2021-08-28 22:53:02 -0300

    Fix auth

--- src/api/v1/auth/controller.js
+++ src/api/v1/auth/controller.js
@@ -3,18 +3,22 @@ const userService = require('../users/services');
 class Auth {
 	login = async (req, res, next) => {
 		try {
-			const users = await userService.findByAttribute({
+			const logged_users = await userService.findByAttribute({
 				attribute: 'username',
 				value: req.body.username
 			});
 
-			if (!users) {
+			if (!logged_users) {
 				res.status(400).send('User not found.');
 			}
-			const [ user ] = users;
+			const [ user ] = logged_users;
 			const auth = user.password === req.body.password;
 
-			auth ? res.json([ ...user.permissions ]) : res.status(400).send('Password incorrect.');
+			const response = {
+				me: user
+			};
+
+			auth ? res.json(response) : res.status(400).send('Password incorrect.');
 		} catch (error) {
 			res.send(error);
 		}
