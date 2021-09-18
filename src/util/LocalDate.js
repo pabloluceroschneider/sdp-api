eada2bc1a5abb1f99ff9e52a960e1208ab16f2be
pabloluceroschneider <pabloluceroschneider@gmail.com>
2021-09-18 15:04:17 -0300

    add miliseconds

--- src/util/LocalDate.js
+++ src/util/LocalDate.js
@@ -9,6 +9,7 @@ const LocalDate = (ISODate = new Date().toISOString()) => {
   ] = ISODate.split("T")[0].split("-");
   const local_hour = new Date(ISODate).getHours() + phase;
   const local_minutes = new Date(ISODate).getMinutes();
+  const local_seconds = new Date(ISODate).getSeconds()
 
   const local_month = month - 1;
   const local_day = day;
@@ -18,7 +19,8 @@ const LocalDate = (ISODate = new Date().toISOString()) => {
     local_month,
     local_day,
     local_hour,
-    local_minutes
+    local_minutes,
+    local_seconds,
   ).toISOString();
   return local_date
 }
