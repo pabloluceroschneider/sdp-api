7019e5b01651c720a466ee430fc41d85d8d85401
pabloluceroschneider <pabloluceroschneider@gmail.com>
2021-09-18 15:16:22 -0300

    Add name validation on company

--- src/api/v1/companies/services.js
+++ src/api/v1/companies/services.js
@@ -1,4 +1,28 @@
 const AbstractService = require('../abstract/AbstractService')
 const schema = require('./model');
 
-module.exports = new AbstractService(schema, 'companies');
+class HistoryService extends AbstractService {
+	constructor(model, collection) {
+		super(model, collection);
+	}
+  
+  create = async ({ body }) => {
+    try {
+        const value = await this.Schema.validateAsync(body);
+
+        const validate = await this.Collection.find(body);
+        if (validate.length) {
+          throw Error(`La empresa ${body.name} ya existe.`)
+        }
+        
+        const inserted = await this.Collection.insert(value);
+        
+        return inserted
+    } catch (error) {
+        throw error
+    }
+};
+
+}
+
+module.exports = new HistoryService(schema, 'companies');
